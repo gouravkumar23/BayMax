d4b1bafd8a4bc32674fe2e2d67629afb91a2a5e4
Gunjari Gourav Kumar <gunjarigourav@gmail.com>
2025-08-13 06:40:33 +0530

    Update firebase.ts

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -2,13 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPXOTNOxUgnrN8RFb0CXCipe2VDhKV-nA",
-  authDomain: "baymax-a5669.firebaseapp.com",
-  projectId: "baymax-a5669",
-  storageBucket: "baymax-a5669.firebasestorage.app",
-  messagingSenderId: "964291241719",
-  appId: "1:964291241719:web:11f74e86cf50d4feb815f2",
-  measurementId: "G-798XFSVP72"
+  Your Credentials Here
 };
 
 const app = initializeApp(firebaseConfig);
